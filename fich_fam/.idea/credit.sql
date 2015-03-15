31dd41699a6c95e95dc763f8026412fa2fc15907
SEBAA Ismail <sebaa.ismail@gmail.com>
2015-03-15 17:25:24 +0100

    sidhmd

---
+++ fich_fam/.idea/credit.sql
@@ -0,0 +1,6 @@
+5000 abs + 3000 fth + 2000 jmel + 4 ml houaria + 1 mil ha
++ 3000 brhim bssri + 1 mil zwich nor + 2000 mstfa bssri
++ 2000 aek f pub + 2 mil hwri wilaya + 2000 bssiri smail +
+1,5 mil hnini + 3000 ali harachi + 500 aek apc
+5650 e+ 5000e + 8000 e ha
+	= 18500 e cmpte ha = my 150 e + 18500 e sidhmd
