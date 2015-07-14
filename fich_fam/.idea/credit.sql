3a5d685b154166c685673e623142ea41178ba23f
SEBAA Ismail <sebaa.ismail@gmail.com>
2015-07-15 01:34:34 +0200

    credit 29500 eur

--- fich_fam/.idea/credit.sql
+++ fich_fam/.idea/credit.sql
@@ -1,7 +1,14 @@
 5000 abs + 3000 fth + 2000 jmel + 4 ml houaria + 1 mil ha
 + 3000 brhim bssri + 1 mil zwich nor + 2000 mstfa bssri
 + 2000 aek f pub + 2 mil hwri wilaya + 2000 bssiri smail +
-1,5 mil hnini + 3000 ali harachi + 500 aek apc
+1,5 mil hnini + 3000 ali harachi + 2 miln ndfa brwiss
 5650 e+ 5000e + 8000 e ha
-	= 19500 e cmpte ha = my 150 e + 19500 e sidhmd --> 04/2015
+	= total 24500 eur (de sid en compte ha ) --> 04/2015
+	c bn mes 250 eur chez moi
 17/04/2015	mm 2 mil
+
+28/06/2015     2000 euro de sidahmed à houaria
+
+08/07/2015	2 miln ndfa brwiss (total 4 mlin)
+
+12/07/2015	3500 eur -->total 29000 eur(de sid en compte ha ) 	
