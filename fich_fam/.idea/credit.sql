f40af6aafc7cec7837e879346bd505965fc1ff84
SEBAA Ismail <sebaa.ismail@gmail.com>
2015-10-26 12:07:13 +0100

    credit my HDD

--- fich_fam/.idea/credit.sql
+++ fich_fam/.idea/credit.sql
@@ -1,7 +1,7 @@
-5000 abs + 3000 fth + 2000 jmel + 4 ml houaria + 1 mil ha
-+ 3000 brhim bssri + 1 mil zwich nor + 2000 mstfa bssri
+ 4 ml houaria + 1 mil ha
++ 3000 brhim bssri + 1 mil zwich nor 
 + 2000 aek f pub + 2 mil hwri wilaya + 2000 bssiri smail +
-1,5 mil hnini + 3000 ali harachi + 2 miln ndfa brwiss
++ 3000 ali harachi + 
 5650 e+ 5000e + 8000 e ha
 	= total 24500 eur (de sid en compte ha ) --> 04/2015
 	c bn mes 250 eur chez moi
@@ -9,6 +9,10 @@
 
 28/06/2015     2000 euro de sidahmed à houaria
 
-08/07/2015	2 miln ndfa brwiss (total 4 mlin)
+08/07/2015	2 miln ndfa brwiss (total 4,5 mlin) ??
+ 
+12/07/2015	3500 eur -->total 29000 eur(de sid en compte ha )
+../09/2015	1 mln + 100 eur rachem 	
 
-12/07/2015	3500 eur -->total 29000 eur(de sid en compte ha ) 	
+-------------------
+my HDD --> 140 e + 1,5 mln 	26/10/2015
