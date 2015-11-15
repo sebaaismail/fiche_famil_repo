ec90ce86384aeeea002e67774ff51c55c9c66c36
SEBAA Ismail <sebaa.ismail@gmail.com>
2015-11-15 18:56:29 +0100

    credit (ouvrt mn cpt + j vrsé 6000)

--- fich_fam/.idea/credit.sql
+++ fich_fam/.idea/credit.sql
@@ -1,9 +1,20 @@
-6000 eur sid	28/10/2015
-31000 + 6000 +2000 ha direct = 37000 + 2000ha =direct eur total = 39000 eur ha
+﻿15/11/2015 :
+ouvrt cpt bnq
+
+6000 = mn cpt
+31000 + 2000 = cpt ha 
+	- - - - - - -> Total dbt sid = 39000 e
+
 my HDD --> 1 mln 	02/11/2015
+
+--------------------------
 
 archive
 ----------------------------
+6000 eur sid	28/10/2015
+31000 + 6000 +2000 ha direct = 37000 + 2000ha =direct eur total = 39000 e ha
+my HDD --> 1 mln 	02/11/2015
+-----------------------------
 my HDD --> 140 e + 1,5 mln 	26/10/2015
  4 ml houaria + 1 mil ha
 + 3000 brhim bssri + 1 mil zwich nor 
@@ -19,6 +30,6 @@ my HDD --> 140 e + 1,5 mln 	26/10/2015
 08/07/2015	2 miln ndfa brwiss (total 4,5 mlin) ??
  
 12/07/2015	3500 eur -->total 29000 eur(de sid en compte ha )
-../09/2015	1 mln + 100 eur rachem 	
-
+../09/2015	1 mln + 150 eur rachem 	
+(mise a jours eur rachem) 15/11/2015
 -------------------
